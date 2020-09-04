ea7e3942030065bf682eea5fc762638b25b10c28
Arda Kaya <ardaka23@outlook.com>
2020-09-05 01:09:09 +0300

    profile box

---
+++ components/profile-box.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import cn from 'classnames'
+import styles from './profile-box.module.css'
+
+import Photo from './photo'
+import { ArrowBottom } from './icons'
+import Button from './button'
+import TextBody from './text-body'
+
+function ProfileBox({ slug, name }) {
+  return (
+    <Button className={cn([styles.box])}>
+      <Photo></Photo>
+      <div className={styles.body}>
+        <TextBody bold>{name}</TextBody>
+        <TextBody className={styles.slug}>@{slug}</TextBody>
+      </div>
+      <ArrowBottom className={styles.icon}></ArrowBottom>
+    </Button>
+  )
+}
+
+export default ProfileBox
