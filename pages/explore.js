2d895aaee1cf1db9146f27a8f81c9f24864381d3
Arda Kaya <ardaka23@outlook.com>
2020-09-16 20:40:22 +0300

    SS

--- pages/explore.js
+++ pages/explore.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-function ExplorerPage() {
+function ExplorePage() {
   return <Layout>Explore</Layout>
 }
 
-export default ExplorerPage
+export default ExplorePage
