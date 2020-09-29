8e0401d6c0ac30895b640bcf0e046fa1478e69da
Arda Kaya <ardaka23@outlook.com>
2020-09-29 17:42:25 +0300

    theme

--- components/navigation.js
+++ components/navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MENU } from '../constants'
 import { useRouter } from 'next/router'
+import cn from 'classnames'
 
 import NavigationButton from './navigation-button'
 import TextTitle from './text-title'
@@ -20,7 +21,7 @@ function Navigation({ flat = false }) {
             notify={menu.notify}
             selected={selected}
             href={menu.path}
-            className={styles.navButton}
+            className={cn(styles.navButton, menu.key)}
           >
             {selected ? menu.iconSelected : menu.icon}
             {showTitle && <TextTitle>{menu.title}</TextTitle>}
---
+++ pages/more.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+import ThemeSelect from '../components/theme-select'
+
+function MorePage() {
+  return (
+    <Layout>
+      <ThemeSelect></ThemeSelect>
+    </Layout>
+  )
+}
+export default MorePage
